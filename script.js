26c50248a1614f6c929c468dc9be74ba95b25e4c
THOMAS-DYLAN <70673340+THOMAS-DYLAN@users.noreply.github.com>
2023-03-08 20:19:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-var hidden = false;
+var hidden = true;
 
 $(".expand").click(
    () => {
       hidden = !hidden;
-      hidden ?
-         $(".links").css({
-            "visibility": "hidden",
-         }) :
+      hidden && (
+         $(".expand").css({ "animation": "arrow .5s backwards"}),
+         $(".links").slideUp(500),
+         setTimeout(() => {
+            $(".links").css({
+               "visibility": "hidden",
+            })
+         }, 505)
+      ) 
+      !hidden && (
          $(".links").css({
             "visibility": "visible",
-         });
+         }),
+         $(".links").hide(),
+         $(".links").slideDown(500),
+         $(".expand").css({ "animation": "arrow .5s forwards" })
+      )
    }
 )
