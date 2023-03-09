25ca4bfc2bd887ebad1a494dd1842b3868c54a30
THOMAS-DYLAN <70673340+THOMAS-DYLAN@users.noreply.github.com>
2023-03-09 16:42:02 -0700

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,21 @@
+$("#home").click(()=>{
+   if(window.location.href == "https://thomas-dylan.github.io/Cotton-Lane-Baptist/"){
+      return;
+   }else{
+      window.location.href = "https://thomas-dylan.github.io/Cotton-Lane-Baptist/"
+   }
+})
+$("#contact").click(()=>{
+   if(window.location.href == "https://thomas-dylan.github.io/Cotton-Lane-Baptist/contact.html"){
+      return;
+   }else{
+      window.location.href = "https://thomas-dylan.github.io/Cotton-Lane-Baptist/contact.html"
+   }
+})
+$("#broadcast").click(()=>{
+   if(window.location.href == "https://thomas-dylan.github.io/Cotton-Lane-Baptist/broadcast.html"){
+      return;
+   }else{
+      window.location.href = "https://thomas-dylan.github.io/Cotton-Lane-Baptist/broadcast.html"
+   }
+})
