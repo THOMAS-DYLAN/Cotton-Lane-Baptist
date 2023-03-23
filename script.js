41101c0aa43029fc693aa761dfe7ec8c77cd6b40
THOMAS-DYLAN <70673340+THOMAS-DYLAN@users.noreply.github.com>
2023-03-23 14:57:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 var hidden = true;
+var events = false;
+$(".event3").hide();
+$(".event4").hide();
+
+$(".eventExpand").click(()=>{
+   events = !events;
+   if(events == false){
+      $(".sundayschool").css({"margin-top": "-8.5%"})
+      $(".secondservice").css({"margin-top": "-8.5%"})
+      $(".event3").hide();
+      $(".event4").hide();
+   }else{
+      $(".sundayschool").css({"margin-top": "-4.5%"})
+      $(".secondservice").css({"margin-top": "-4.5%"})
+      $(".event3").show();
+      $(".event4").show();
+   }
+})
 
 $(".expand").click(
    () => {
